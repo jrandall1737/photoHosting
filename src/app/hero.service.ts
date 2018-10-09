c4b6740e8822b4f6ac01b0ca6e783971d4f93563
Jeff Randall <jeffrey.randall@keysight.com>
2018-10-08 21:22:37 -0700

    done with services

---
+++ src/app/hero.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+import { Observable, of } from 'rxjs';
+
+// When you provide the service at the root level, Angular creates a single,
+// shared instance of HeroService and injects into any class that asks for it.
+// Registering the provider in the @Injectable metadata also allows Angular to
+// optimize an app by removing the service if it turns out not to be used after all.
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  // getHeroes(): Hero[] {
+  //   return HEROES;
+  // }
+
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+}
